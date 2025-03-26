b797491b15c1eb8d2ff6f8ba3c039250d019b237
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-26 19:50:44 +0100

    [UPD]  Grafica

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 
 function calcola(){
     let km = parseFloat(document.getElementById('km-field').value);
-    let anni = parseInt(document.getElementById('anni-field').value, 10);
+    let anni = document.getElementById('anni-field').value;
 
     if (isNaN(km) || km <= 0) {
         console.error("Inserire un valore valido per i chilometri.");
         alert("Inserire un valore valido per i chilometri.");
         return;
     }
-
-    if (isNaN(anni) || anni <= 0) {
-        console.error("Inserire un valore valido per l'età.");
-        alert("Inserire un valore valido per l'età.");
+    if (anni !== 'Minorenne' && anni !== 'Maggiorenne' && anni !== 'Pensionato') {
+        console.error("Inserire un'età valida.");
+        alert("Inserire un'età valida.");
         return;
     }
 
     let price = (km * 0.21);
     let finalPrice;
-    if (anni < 18) {
+
+    if (anni === 'Minorenne') {
         finalPrice = price - (price * 20 / 100);
-    } else if (anni >= 65) {
+    } else if (anni === 'Pensionato') {
         finalPrice = price - (price * 40 / 100);
     } else {
         finalPrice = price;
